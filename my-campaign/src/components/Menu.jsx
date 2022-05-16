a9b97ebab4ba727068a0b2097c1bdf6772f32d7a
Luis porto <lmportoesq@gmail.com>
2022-05-16 18:54:53 -0500

    feac: add styles to HomePage and Menu components

--- my-campaign/src/components/Menu.jsx
+++ my-campaign/src/components/Menu.jsx
@@ -1,6 +1,6 @@
 function Menu() {
     return (
-        <div className="mx-auto">
+        <div className="bg-gray-200 text-right mx-auto">
             <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" >Datos de la campaña</button>
             <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit">Ingresar líderes</button>
             <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit">Poblaciones</button>
--- my-campaign/src/pages/HomePage.jsx
+++ my-campaign/src/pages/HomePage.jsx
@@ -3,9 +3,9 @@ import logo from '../images/Logo.jpg';
 
 function HomePage() {
     return (
-        <div className='container mx-auto w-3/4 bg-blue-400'>
+        <div className='container mx-auto w-3/4 h-[38rem] bg-blue-400'>
             <Menu />
-            <div className='w-64 h-96 mx-auto mt-10'>
+            <div className='w-64 h-96 mx-auto my-auto mt-10'>
                 <img src={logo} alt='logo'  />
             </div>
         </div>
