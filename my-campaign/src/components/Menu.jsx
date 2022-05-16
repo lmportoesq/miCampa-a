202fa1563f31495774f65d26332fa935b8598712
Luis porto <lmportoesq@gmail.com>
2022-05-16 17:45:05 -0500

    feac: create component Menu and call it from HomePage

---
+++ my-campaign/src/components/Menu.jsx
@@ -0,0 +1,12 @@
+function Menu() {
+    return (
+        <div className="mx-auto">
+            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" >Datos de la campaña</button>
+            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit">Ingresar líderes</button>
+            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit">Poblaciones</button>
+            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit">Informes</button>
+            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit">Salir</button>
+        </div>
+    )
+}
+export default Menu;
--- my-campaign/src/pages/HomePage.jsx
+++ my-campaign/src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
+import Menu from '../components/Menu';
 import logo from '../images/Logo.jpg';
 
 function HomePage() {
     return (
         <div className='container mx-auto w-3/4 bg-blue-400'>
+            <Menu />
             <div className='w-64 h-96 mx-auto mt-10'>
                 <img src={logo} alt='logo'  />
             </div>
