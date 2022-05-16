5926607f8cff848db3cbe9594c2d13d3feff87c4
Luis porto <lmportoesq@gmail.com>
2022-05-16 16:48:43 -0500

    feac: se eliminan imagenes para HomePage y se cambia diseño

--- my-campaign/src/pages/HomePage.jsx
+++ my-campaign/src/pages/HomePage.jsx
@@ -1,16 +1,10 @@
 import logo from '../images/Logo.jpg';
-import opcion1 from '../images/candidato3.png';
-import opcion2 from '../images/Lideres.png';
 
 function HomePage() {
     return (
-        <div className='container w=120px h=120px'>
-            <div className='w=120px h=120px'>
+        <div className='container mx-auto w-3/4 bg-blue-400'>
+            <div className='w-64 h-96 mx-auto mt-10'>
                 <img src={logo} alt='logo'  />
-            </div>
-            <div>
-                <img src={opcion1} alt='opcion1' />
-                <img src={opcion2} alt='opcion2' />
             </div>
         </div>
     )
