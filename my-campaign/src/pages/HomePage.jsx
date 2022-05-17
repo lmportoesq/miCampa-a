f1778e391c270460f4e8d2ad362f64987a077b5a
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2022-05-16 19:51:11 -0500

    feac: create Footer component and call it from HomePage (#5)
    Co-authored-by: Luis porto <lmportoesq@gmail.com>

--- my-campaign/src/pages/HomePage.jsx
+++ my-campaign/src/pages/HomePage.jsx
@@ -1,14 +1,16 @@
+import Footer from '../components/Footer';
 import Menu from '../components/Menu';
 import logo from '../images/Logo.jpg';
 
 function HomePage() {
     return (
-        <div className='container mx-auto w-3/4 h-[38rem] bg-blue-400'>
+        <div className='container mx-auto w-3/4 bg-blue-400 mt-10'>
             <Menu />
-            <div className='w-64 h-96 mx-auto my-auto mt-10'>
-                <img src={logo} alt='logo'  />
+            <div className='w-[32rem] h-[30rem] mx-auto my-auto'>
+                <img src={logo} alt='logo' className='w-full h-[28rem] mt-10'  />
             </div>
+            <Footer />
         </div>
-    )
+    )    
 }
 export default HomePage;
