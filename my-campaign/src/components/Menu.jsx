891e6f73480c5c4544b501dcaad22633bbe87151
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2022-05-23 21:42:55 -0500

    Feac/campaign (#7)
    * call CreateCampaign component
    
    * call Routes component
    
    Co-authored-by: Luis porto <lmportoesq@gmail.com>

--- my-campaign/src/components/Menu.jsx
+++ my-campaign/src/components/Menu.jsx
@@ -1,7 +1,10 @@
 function Menu() {
+    function handleClick(){
+        window.location.href = '/create-campaign';
+    }
     return (
         <div className="bg-gray-200 text-right mx-auto">
-            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" >Datos de la campaña</button>
+            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" onClick={handleClick} >Datos de la campaña</button>
             <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit">Ingresar líderes</button>
             <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit">Poblaciones</button>
             <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit">Informes</button>
