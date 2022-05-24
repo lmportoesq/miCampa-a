c17c2a9582957ee2023796adc62a24d2f7e0796e
Luis porto <lmportoesq@gmail.com>
2022-05-24 18:35:55 -0500

    feac: create register user page

--- my-campaign/src/pages/CreateUserPage.jsx
+++ my-campaign/src/pages/CreateUserPage.jsx
@@ -1,6 +1,6 @@
 import Footer from '../components/Footer';
 import GetHome from '../components/GetHome';
-import CreateUsers from '../components/CreateAdmin';
+import CreateUsers from '../components/CreateUsers';
 
 function CreateUserPage() {
     return (
