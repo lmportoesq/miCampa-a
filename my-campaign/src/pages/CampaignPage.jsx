757da62c149f205fbd1ac6f56de47f27605dc939
Luis porto <lmportoesq@gmail.com>
2022-05-24 14:50:33 -0500

    fix: CRLF to LF

---
+++ my-campaign/src/pages/CampaignPage.jsx
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import GetHome from '../components/GetHome';
+import CreateCampaign from '../components/CreateCampaign';
+
+function CampaignPage() {
+    return (
+        <div className='container mx-auto w-3/4 bg-blue-400 mt-10'>
+            <GetHome />
+            <CreateCampaign />
+            <Footer />
+        </div>
+    )    
+}
+export default CampaignPage;
