e14326ac0720c728ce6e104f94810024c44ca63a
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2022-05-24 18:37:39 -0500

    Feac/reg user page (#12)
    * feac: create CreateUserPage and CreateUser components
    
    * feac: call CreateUserPage from Menu and Routes
    
    * feac: create register user page
    
    Co-authored-by: Luis porto <lmportoesq@gmail.com>

---
+++ my-campaign/src/components/CreateUsers.jsx
@@ -0,0 +1,14 @@
+function CreateUser() {
+    return (
+        <div className='container mx-auto w-3/4 mt-5'>
+            <form className='shadow-lg shadow-blue-900 mt-5 border-red-300 w-[32rem] h-[30rem] mx-auto my-auto grid grid-col-1 gap-3 text-center p-5 rounded-lg'>
+                <h1 className='shadow-md '>Registro de líderes</h1>
+                <input className='border rounded border-black p-1' type="text" placeholder="Primer nombre"/>
+                <input className='border rounded border-black p-1' type="text" placeholder="Segundo nombre"/>
+                <input className='border rounded border-black p-1' type="email" placeholder="Email"/>
+                <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit">Enviar</button>
+            </form>
+        </div>
+    )
+}
+export default CreateUser;
