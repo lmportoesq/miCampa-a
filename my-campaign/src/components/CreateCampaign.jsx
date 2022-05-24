4a7ff50e1330dc0f6c4f92bc4171b5b9321774be
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2022-05-24 14:09:07 -0500

    Feac/campaign (#8)
    * call CreateCampaign component
    
    * call Routes component
    
    * fix: CRLF to LF
    
    Co-authored-by: Luis porto <lmportoesq@gmail.com>

--- my-campaign/src/components/CreateCampaign.jsx
+++ my-campaign/src/components/CreateCampaign.jsx
@@ -1,21 +1,20 @@
 function CreateCampaign() {
     return (
-        <div>
-            <h1>Datos de la campaña</h1>
-            <hr></hr>
-            <form>
-                <input type="text" placeholder="Nombre del candidato"></input>
-                <input type="text" placeholder="Slogan de la campaña"></input>
-                <input type="text" placeholder="Dirección del comando"></input>
-                <input type="text" placeholder="Teléfono del comando"></input>
+        <div className='container mx-auto w-3/4 mt-10'>
+            <form className='shadow-lg shadow-blue-900 mt-5 border-red-300 w-[32rem] h-[30rem] mx-auto my-auto grid grid-col-1 gap-3 text-center p-5 rounded-lg'>
+                <h1 className='shadow-md'>Datos de la campaña</h1>
+                <input className='border rounded border-black p-1' type="text" placeholder="Nombre del candidato"/>
+                <input className='border rounded border-black p-1' type="text" placeholder="Slogan de la campaña"/>
+                <input className='border rounded border-black p-1' type="text" placeholder="Dirección del comando"/>
+                <input className='border rounded border-black p-1' type="text" placeholder="Teléfono del comando"/>
                 <label htmlFor="type">Tipo de campaña</label>
-                <select name="type">
+                <select name="type" className='border rounded border-black p-1'>
                     <option value="alcaldia">Alcaldía</option>
                     <option value="gobernacion">Gobernación</option>
                     <option value="camara">Cámara</option>
                     <option value="senado">Senado</option>
                 </select>
-                <button type="submit">Enviar</button>
+                <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit">Enviar</button>
             </form>
         </div>
     )
