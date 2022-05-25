773d5026947919cbc15c0bca0da14cd91b9423ee
Luis porto <lmportoesq@gmail.com>
2022-05-25 10:34:03 -0500

    feac: configuring tailwind for customs styles

--- my-campaign/src/components/Login.jsx
+++ my-campaign/src/components/Login.jsx
@@ -3,8 +3,8 @@ function Login() {
         <div className='container mx-auto w-3/4 mt-5'>
             <form className='shadow-lg shadow-blue-900 mt-5 border-red-300 w-[32rem] h-[30rem] mx-auto my-auto grid grid-col-1 gap-3 text-center p-5 rounded-lg'>
                 <h1 className='shadow-md '>Login</h1>
-                <input className='border rounded border-black p-1 h-12' type="email" placeholder="Email"/>
-                <input className='border rounded border-black p-1 h-12' type="password" placeholder="Password"/>
+                <input className='input' type="email" placeholder="Email"/>
+                <input className='input' type="password" placeholder="Password"/>
                 <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit">Enviar</button>
             </form>
         </div>
