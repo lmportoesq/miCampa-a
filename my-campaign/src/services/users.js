973a8a57d48def3e6934be8b253d8b1c1eae4baf
Luis porto <lmportoesq@gmail.com>
2022-05-27 23:33:18 -0500

    feac/create messages with sweetalert2

---
+++ my-campaign/src/services/users.js
@@ -0,0 +1,58 @@
+//const API_URL = process.env.REACT_APP_URL;
+const API_URL = 'http://localhost:8080';
+console.log('La ruta a la API es..',API_URL)
+export async function createUser(user) {
+ 
+  const payload = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  };
+  try {
+    const response = await fetch(`${API_URL}/api/users`, payload);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export async function updateUser(user) {
+  const payload = {
+    method: 'PATCH',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  };
+
+  try {
+    const response = await fetch(`${API_URL}/api/users/${user.id}`, payload);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export async function getAllUsers() {
+  try {
+    const response = await fetch(`${API_URL}/api/users`);
+    const users = await response.json();
+    return users;
+  } catch (error) {
+    return null;
+  }
+}
+
+export async function filteringUserByEmail(email) {
+  try {
+    const response = await fetch(`${API_URL}/api/users?email=${email}`);
+    const user = await response.json();
+    return user;
+  } catch (error) {
+    return null;
+  }
+}
