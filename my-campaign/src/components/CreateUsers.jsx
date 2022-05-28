7e637c2cb51e1a65e3fd8d7ac6fa09fd314ea2e2
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2022-05-27 21:33:41 -0500

    Feac/validate (#18)
    * feac: create validations
    
    * feac: create validations
    
    * feac: create validations
    
    Co-authored-by: Luis porto <lmportoesq@gmail.com>

--- my-campaign/src/components/CreateUsers.jsx
+++ my-campaign/src/components/CreateUsers.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
+
 function CreateUser() {
+    const [data, setData] = useState({
+        name:'',
+        lastname:'',
+        email:'',
+    });
+   
+    const handleValidate = () => {
+        const { name, lastname, email } = data;
+        const valido = !name.length || !lastname.length || !email.length;
+        return valido;
+    };
+
+    function handleChange(e) {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value,
+        });
+    }
+    
     return (
         <div className='container mx-auto w-3/4 mt-5'>
             <form className='shadow-lg shadow-blue-900 mt-5 border-red-300 w-[32rem] h-[30rem] mx-auto my-auto grid grid-col-1 gap-3 text-center p-5 rounded-lg'>
                 <h1 className='shadow-md '>Registro de líderes</h1>
-                <input className='border rounded border-black p-1' type="text" placeholder="Primer nombre"/>
-                <input className='border rounded border-black p-1' type="text" placeholder="Segundo nombre"/>
-                <input className='border rounded border-black p-1' type="email" placeholder="Email"/>
-                <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit">Enviar</button>
+                <input className='border rounded border-black p-1' name="name" type="text" placeholder="Primer nombre" onChange={handleChange}/>
+                <input className='border rounded border-black p-1' name="lastname" type="text" placeholder="Segundo nombre" onChange={handleChange}/>
+                <input className='border rounded border-black p-1' name="email" type="email" placeholder="Email" onChange={handleChange}/>
+                <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" disabled={handleValidate()}>Enviar</button>
             </form>
         </div>
     )
