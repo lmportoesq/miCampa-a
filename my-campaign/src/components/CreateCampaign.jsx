2847082444552073e496297ab98d33cb72cee82d
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2022-05-29 20:31:35 -0500

    Feac/save data campaign (#21)
    * feac: add sweetalert messages
    
    * feac: connect createCampaign to back-end
    
    Co-authored-by: Luis porto <lmportoesq@gmail.com>

--- my-campaign/src/components/CreateCampaign.jsx
+++ my-campaign/src/components/CreateCampaign.jsx
@@ -1,36 +1,78 @@
 import { useState } from "react";
+//import Swal from "sweetalert2";
+import { createCampaign } from '../services/campaigns'
 
 function CreateCampaign() {
     const [data, setData] = useState({
-        name:'',
-        slogan:'',
-        adress:'',
-        phone:'',
+        name: '',
+        slogan: '',
+        adress: '',
+        phone: '',
     });
 
     const handleValidate = () => {
-        const { name, slogan, adress, phone} = data;
+        const { name, slogan, adress, phone } = data;
         const valido = !name.length || !slogan.length || !adress.length || !phone.length;
+        console.log('resultado de valido...', valido)
         return valido;
     };
 
     const handleInputChange = (e) => {
         setData({
-          ...data,
-          [e.target.name]: e.target.value,
+            ...data,
+            [e.target.name]: e.target.value,
         });
+        console.log('Alguien escribe...', data)
     };
+    //const API_URL=process.env.REACT_APP_URL;
+    //const API_URL='http://localhost:8080'
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const token = localStorage.getItem('token');
+            if (!token) {
+                alert('Debe estar logeado para ingresar datos de la campaña')
+                return;
+            }
+            await createCampaign(data);
+
+            //console.log('El resultado de response es ...', response)
+            //console.log('El role es ...',profile.role)
+
+            //localStorage.setItem('token', token);
+            //dispatch(saveAdminData({ profile, id }));
+        /*    if (response.status === 500) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Algo salió mal',
+                    text: 'Usuario o contraseña no válida...',
+                });
+            } else {
+                Swal.fire(
+                    'Login exitoso ',
+                    'Usuario autenticado corréctamente...!',
+                    'success',
+                );
+                // window.location.href = '/home';
+                //profile.role === 'admin' ? navigate('/AdminPage') : navigate('/Search');
+            }
+            // setShowForm(false); */
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
 
     return (
         <div className='container mx-auto mt-5'>
-            <form className='shadow-lg shadow-blue-900 border-red-300 w-[32rem] h-[30rem] mx-auto my-auto grid grid-col-1 gap-3 text-center p-5 rounded-lg'>
+            <form className='shadow-lg shadow-blue-900 border-red-300 w-[32rem] h-[30rem] mx-auto my-auto grid grid-col-1 gap-3 text-center p-5 rounded-lg' onSubmit={handleSubmit}>
                 <h1 className='shadow-md'>Datos de la campaña</h1>
-                <input className='border rounded border-black p-1' type="text" name="name" placeholder="Nombre del candidato"  onChange={handleInputChange} />
-                <input className='border rounded border-black p-1' type="text" name="slogan" placeholder="Slogan de la campaña"  onChange={handleInputChange} />
+                <input className='border rounded border-black p-1' type="text" name="name" placeholder="Nombre del candidato" onChange={handleInputChange} />
+                <input className='border rounded border-black p-1' type="text" name="slogan" placeholder="Slogan de la campaña" onChange={handleInputChange} />
                 <input className='border rounded border-black p-1' type="text" name="adress" placeholder="Dirección del comando" onChange={handleInputChange} />
-                <input className='border rounded border-black p-1' type="text" name="phone" placeholder="Teléfono del comando"  onChange={handleInputChange} />
+                <input className='border rounded border-black p-1' type="text" name="phone" placeholder="Teléfono del comando" onChange={handleInputChange} />
                 <label htmlFor="type">Tipo de campaña</label>
-                <select name="type" className='border rounded border-black p-1'  onChange={handleInputChange}>
+                <select name="type" className='border rounded border-black p-1' onChange={handleInputChange}>
                     <option value="alcaldia">Alcaldía</option>
                     <option value="gobernacion">Gobernación</option>
                     <option value="camara">Cámara</option>
---
+++ my-campaign/src/services/campaigns.js
@@ -0,0 +1,51 @@
+const API_URL_ENV = process.env.REACT_APP_URL;
+const API_URL = 'http://localhost:8080';
+
+export async function createCampaign(campaign) { 
+  console.log('La data recibida es.. ',campaign)
+  console.log('La url es ', API_URL)
+  const payload = {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(campaign)
+  };
+  try {
+    const response = await fetch(`${API_URL}/api/campaigns`, payload);
+    console.log('REsultado del fetch es..',response)
+    const data = await response.json();
+    console.log('REsultado del servicio createCAmpaig es..',data)
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export async function updateUser(campaign) {
+  const payload = {
+    method: 'PATCH',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(campaign),
+  };
+
+  try {
+    const response = await fetch(`${API_URL}/api/campaigns/${campaign.id}`, payload);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export async function getAllCampaigns() {
+  try {
+    const response = await fetch(`${API_URL}/api/campaigns`);
+    const campaigns = await response.json();
+    return campaigns;
+  } catch (error) {
+    return null;
+  }
+}
