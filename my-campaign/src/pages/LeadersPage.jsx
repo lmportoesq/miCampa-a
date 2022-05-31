4f90abcf2e31065627c0fdc45c75befbe64c7be3
Luis porto <lmportoesq@gmail.com>
2022-05-30 22:55:04 -0500

    Feac: list-leaders

--- my-campaign/src/pages/LeadersPage.jsx
+++ my-campaign/src/pages/LeadersPage.jsx
@@ -5,10 +5,7 @@ function LeadersPage({ data }) {
         <div className='rounded shadow-blue-900 p-2 grid grid-cols-1 gap-2 h-[28rem] text-center'>
             <img className='w-[12rem] h-[12rem] mx-auto' src={logo} alt='logo' />
             <div className='text-center'>
-                <h1 className='text-2xl text-bold'>{data.nombre}</h1>
-                <h2 className='text-xl'>Cédula: {data.cedula}</h2>
-                <h2 className='text-xl'>Direción: {data.direccion}</h2>
-                <h2 className='text-xl'>Teléfono: {data.telefono}</h2>
+                <h1 className='text-2xl text-bold'>{data.firstName} {data.lastName}</h1>
                 <h2 className='text-xl'>Tipo de líder: {data.tipo}</h2>
             </div>
         </div>
