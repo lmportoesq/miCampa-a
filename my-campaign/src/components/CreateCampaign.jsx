315fb9c376d154fd60bcc8cce83c1b85ead486ee
Luis porto <lmportoesq@gmail.com>
2022-05-30 19:57:18 -0500

    feac: sava data campaign with axios

--- my-campaign/src/components/CreateCampaign.jsx
+++ my-campaign/src/components/CreateCampaign.jsx
@@ -1,19 +1,18 @@
 import { useState } from "react";
-//import Swal from "sweetalert2";
-import { createCampaign } from '../services/campaigns'
+import Swal from "sweetalert2";
+import clienteAxios from  '../config/axios';
 
 function CreateCampaign() {
     const [data, setData] = useState({
-        name: '',
-        slogan: '',
-        adress: '',
-        phone: '',
+        campaignType: '',
+        candidateName: '',
+        campaignSlogan: '',
+        campaignAdress:'',
     });
 
     const handleValidate = () => {
-        const { name, slogan, adress, phone } = data;
-        const valido = !name.length || !slogan.length || !adress.length || !phone.length;
-        console.log('resultado de valido...', valido)
+        const { campaignType, candidateName, campaignSlogan, campaignAdress } = data;
+        const valido = !campaignType.length || !candidateName.length || !campaignSlogan.length || !campaignAdress.length;
         return valido;
     };
 
@@ -21,62 +20,43 @@ function CreateCampaign() {
         setData({
             ...data,
             [e.target.name]: e.target.value,
-        });
-        console.log('Alguien escribe...', data)
-    };
-    //const API_URL=process.env.REACT_APP_URL;
-    //const API_URL='http://localhost:8080'
-    const handleSubmit = async (e) => {
+        });    
+    }
+
+    const handleSubmit = async (e) =>{
         e.preventDefault();
-        try {
-            const token = localStorage.getItem('token');
-            if (!token) {
-                alert('Debe estar logeado para ingresar datos de la campaña')
-                return;
-            }
-            await createCampaign(data);
-
-            //console.log('El resultado de response es ...', response)
-            //console.log('El role es ...',profile.role)
-
-            //localStorage.setItem('token', token);
-            //dispatch(saveAdminData({ profile, id }));
-        /*    if (response.status === 500) {
+        await clienteAxios.post('/api/campaigns',data)
+        .then (res=>{
+            if (res.data.code === 11000) {
                 Swal.fire({
                     icon: 'error',
                     title: 'Algo salió mal',
-                    text: 'Usuario o contraseña no válida...',
+                    text: 'Error del servidor...',
                 });
-            } else {
+            }else {
                 Swal.fire(
-                    'Login exitoso ',
-                    'Usuario autenticado corréctamente...!',
+                    'Se agregaron los datos de la campaña corréctamente ',
+                    res.data.mensaje,
                     'success',
                 );
-                // window.location.href = '/home';
-                //profile.role === 'admin' ? navigate('/AdminPage') : navigate('/Search');
             }
-            // setShowForm(false); */
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
+        })
+    }
 
     return (
         <div className='container mx-auto mt-5'>
             <form className='shadow-lg shadow-blue-900 border-red-300 w-[32rem] h-[30rem] mx-auto my-auto grid grid-col-1 gap-3 text-center p-5 rounded-lg' onSubmit={handleSubmit}>
                 <h1 className='shadow-md'>Datos de la campaña</h1>
-                <input className='border rounded border-black p-1' type="text" name="name" placeholder="Nombre del candidato" onChange={handleInputChange} />
-                <input className='border rounded border-black p-1' type="text" name="slogan" placeholder="Slogan de la campaña" onChange={handleInputChange} />
-                <input className='border rounded border-black p-1' type="text" name="adress" placeholder="Dirección del comando" onChange={handleInputChange} />
-                <input className='border rounded border-black p-1' type="text" name="phone" placeholder="Teléfono del comando" onChange={handleInputChange} />
+                <input className='border rounded border-black p-1' type="text" name="candidateName" placeholder="Nombre del candidato" onChange={handleInputChange} />
+                <input className='border rounded border-black p-1' type="text" name="campaignSlogan" placeholder="Slogan de la campaña" onChange={handleInputChange} />
+                <input className='border rounded border-black p-1' type="text" name="campaignAdress" placeholder="Dirección del comando" onChange={handleInputChange} />
                 <label htmlFor="type">Tipo de campaña</label>
-                <select name="type" className='border rounded border-black p-1' onChange={handleInputChange}>
-                    <option value="alcaldia">Alcaldía</option>
-                    <option value="gobernacion">Gobernación</option>
-                    <option value="camara">Cámara</option>
-                    <option value="senado">Senado</option>
+                <select name="campaignType" className='border rounded border-black p-1' onChange={handleInputChange}>
+                    <option value=""></option>
+                    <option value="Alcaldía">Alcaldía</option>
+                    <option value="Gobernación">Gobernación</option>
+                    <option value="Cámara">Cámara</option>
+                    <option value="Senado">Senado</option>
                 </select>
                 <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" disabled={handleValidate()}>Enviar</button>
             </form>
