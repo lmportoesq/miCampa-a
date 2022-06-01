04397839529cfc280ea02e72cffa1ce87c56b4c4
Luis porto <lmportoesq@gmail.com>
2022-06-01 16:48:26 -0500

    fix: eslint rules

--- my-campaign/src/services/campaigns.js
+++ my-campaign/src/services/campaigns.js
@@ -2,8 +2,6 @@ const API_URL_ENV = process.env.REACT_APP_URL;
 const API_URL = 'http://localhost:8080';
 
 export async function createCampaign(campaign) { 
-  console.log('La data recibida es.. ',campaign)
-  console.log('La url es ', API_URL)
   const payload = {
     method: 'POST',
     headers: {
@@ -11,12 +9,10 @@ export async function createCampaign(campaign) {
     },
     body: JSON.stringify(campaign),
   };
-  console.log('Este es el Payload:',payload)
+  
   try {
     const response = await fetch(`${API_URL}/api/campaigns`, payload);
-    console.log('REsultado del fetch es..',response)
     const data = await response.json();
-    console.log('REsultado del servicio createCAmpaig es..',data)
     return data;
   } catch (error) {
     throw new Error(error);
