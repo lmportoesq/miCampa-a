f78fcf40367d7365d777ab5236e65cd4b596b152
Luis porto <lmportoesq@gmail.com>
2022-06-02 11:05:01 -0500

    Fix: Resolving problems

--- my-campaign/src/components/CreateLeaders.jsx
+++ my-campaign/src/components/CreateLeaders.jsx
@@ -1,16 +1,16 @@
 import { useState } from "react";
 import Swal from "sweetalert2";
-import clientAxios from  '../config/axios';
+import clienteAxios from '../config/axios';
 
 function CreateLeader() {
-    const userID = localStorage.getItem('Id');
+    const userID = localStorage.getItem('id');
     const [data, setData] = useState({
         doctIdent: '',
         leaderType: '',
         phoneNumber: '',
-        user: userID
+        // user: userID
     });
-    
+
     const handleValidate = () => {
         const { doctIdent, leaderType, phoneNumber } = data;
         const valido = !doctIdent.length || !leaderType.length || !phoneNumber.length;
@@ -21,46 +21,85 @@ function CreateLeader() {
         setData({
             ...data,
             [e.target.name]: e.target.value,
-        });    
-        console.log('Contenido de data es..',data)
+        });
     }
-
-    const handleSubmit = async (e) =>{
+    const API_URL = 'http://localhost:8080'
+    const handleSubmit = async (e) => {
+        console.log('Estoy en handlesubmit y Contenido de data es..', data)
         e.preventDefault();
-        await clientAxios.post('/api/leaders',data)
-        .then (res=>{
-            if (res.data.code === 11000) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Algo salió mal',
-                    text: 'Error del servidor...',
-                });
-            }else {
-                Swal.fire(
-                    'Se agregaron los datos de la campaña corréctamente ',
-                    res.data.mensaje,
-                    'success',
-                );
-            }
-        })
-    }
+       // fetch('http://localhost:8080/api/leaders')
+       //     .then(response => response.json())
+       //     .then(data => console.log('Contenido de data es....',data))
+       //     .catch (error => console.log(error))
+      try {
+          const response = await fetch('http://localhost:8080/api/leaders', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(data),
+          });
+        //  const { doctIdent, leaderType, phoneNumber } = await response.json();
+          console.log('El contenido de data es..',data)
+          //const { token, profile, id } = await response.json();
+          console.log('El contenido de response es..',response)
+          if (response.status === 500) {
+              Swal.fire({
+                  icon: 'error',
+                  title: 'Algo salió mal',
+                  text: 'Usuario o contraseña no válida...',
+              });
+              return;
+          }
 
-    return (
-        <div className='container mx-auto mt-5'>
-            <form className='shadow-lg shadow-blue-900 border-red-300 w-[32rem] h-[30rem] mx-auto my-auto grid grid-col-1 gap-3 text-center p-5 rounded-lg' onSubmit={handleSubmit}>
-                <h1 className='shadow-md'>Datos personales del líder</h1>
-                <input className='border rounded border-black p-1' type="text" name="doctIdent" placeholder="Cédula No." onChange={handleInputChange} />
-                <input className='border rounded border-black p-1' type="text" name="phoneNumber" placeholder="Teléfono" onChange={handleInputChange} />
-                <label htmlFor="type">Tipo de líder</label>
-                <select name="leaderType" className='border rounded border-black p-1' onChange={handleInputChange}>
-                    <option value=""></option>
-                    <option value="Aspirante al concejo">Aspirante al concejo</option>
-                    <option value="Comunitario">Comunitario</option>
-                    <option value="Otro">Otro</option>
-                </select>
-                <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" disabled={handleValidate()}>Enviar</button>
-            </form>
-        </div>
-    )
+          if (response.status === 200) {
+              Swal.fire(
+                  'Login exitoso ',
+                  'Usuario autenticado corréctamente...!',
+                  'success',
+              );
+          }
+
+      } catch (error) {
+          console.log(error);
+      } 
+
+    /*  const response = await clienteAxios.post('/api/leaders',data)
+      console.log('REsultado de axios es',response)
+      .then (res=>{
+          if (res.data.code === 11000 || res.data.code === 500) {
+              Swal.fire({
+                  icon: 'error',
+                  title: 'Algo salió mal',
+                  text: 'Error del servidor...',
+              });
+          }else {
+              Swal.fire(
+                  'Se agregaron los datos de la campaña corréctamente ',
+                  res.data.mensaje,
+                  'success',
+              );
+          }
+      })
+      .catch(error) */
+}
+
+return (
+    <div className='container mx-auto mt-5'>
+        <form className='shadow-lg shadow-blue-900 border-red-300 w-[32rem] h-[30rem] mx-auto my-auto grid grid-col-1 gap-3 text-center p-5 rounded-lg' onSubmit={handleSubmit}>
+            <h1 className='shadow-md'>Datos del líder</h1>
+            <input className='border rounded border-black p-1' type="number" name="doctIdent" placeholder="Cédula No." onChange={handleInputChange} />
+            <input className='border rounded border-black p-1' type="tel" name="phoneNumber" placeholder="Teléfono" onChange={handleInputChange} />
+            <label htmlFor="type">Tipo de líder</label>
+            <select name="leaderType" className='border rounded border-black p-1' onChange={handleInputChange}>
+                <option value=""></option>
+                <option value="Aspirante al concejo">Aspirante al concejo</option>
+                <option value="Comunitario">Comunitario</option>
+                <option value="Otro">Otro</option>
+            </select>
+            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" disabled={handleValidate()}>Enviar</button>
+        </form>
+    </div>
+)
 }
 export default CreateLeader;
