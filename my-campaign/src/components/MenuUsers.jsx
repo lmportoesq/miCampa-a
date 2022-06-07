b98b88c1486c56a14c0c5e26ea40260680f8aad9
Luis porto <lmportoesq@gmail.com>
2022-06-06 19:48:17 -0500

    Feac: create routes to CreateFollowerPage

--- my-campaign/src/components/MenuUsers.jsx
+++ my-campaign/src/components/MenuUsers.jsx
@@ -2,8 +2,8 @@ function MenuUsers() {
     function handleClickPersonal(){
         window.location.href = '/create-leader';
     }
-    function handleClickRegPlan(){
-        window.location.href = '/create-plan'
+    function handleClickFollower(){
+        window.location.href = '/create-follower'
     }
     function handleClickListPlan(){
         window.location.href = '/query-leader'
@@ -16,7 +16,7 @@ function MenuUsers() {
     return (
         <div className="bg-gray-200 text-right mx-auto">
             <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" onClick={handleClickPersonal} >Datos personales</button>
-            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" onClick={handleClickRegPlan}>Ingresar planillas</button>
+            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" onClick={handleClickFollower}>Ingresar seguidores</button>
             <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit">Listar rechazados</button>
             <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" onClick={handleClickListPlan}>Listar planillas</button>
             <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" onClick={handleClickLogout}>Salir</button>
---
+++ my-campaign/src/pages/CreateFollowersPage.jsx
@@ -0,0 +1,14 @@
+import Footer from '../components/Footer';
+import GetHome from '../components/GetHome';
+import CreateFollowerPage from '../components/CreateFollowers';
+
+function CreateFollower() {
+    return (
+        <div className='container mx-auto'>
+            <GetHome />
+            <CreateFollowerPage />
+            <Footer />
+        </div>
+    )    
+}
+export default CreateFollower;
