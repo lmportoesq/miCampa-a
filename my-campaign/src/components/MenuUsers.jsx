e294ffb3fa754be775f9ecf1981302a7438fde60
Luis porto <lmportoesq@gmail.com>
2022-06-07 13:50:02 -0500

    feac: create menu to list followers

--- my-campaign/src/components/MenuUsers.jsx
+++ my-campaign/src/components/MenuUsers.jsx
@@ -5,8 +5,8 @@ function MenuUsers() {
     function handleClickFollower(){
         window.location.href = '/create-follower'
     }
-    function handleClickListPlan(){
-        window.location.href = '/query-leader'
+    function handleClickListFollowers(){
+        window.location.href = '/query-followers'
     }
     function handleClickLogout(){
         localStorage.removeItem('token');
@@ -17,8 +17,8 @@ function MenuUsers() {
         <div className="bg-gray-200 text-right mx-auto">
             <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" onClick={handleClickPersonal} >Datos personales</button>
             <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" onClick={handleClickFollower}>Ingresar seguidores</button>
+            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" onClick={handleClickListFollowers}>Listar seguidores</button>
             <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit">Listar rechazados</button>
-            <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" onClick={handleClickListPlan}>Listar planillas</button>
             <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-2 m-2" type="submit" onClick={handleClickLogout}>Salir</button>
         </div>
     )
