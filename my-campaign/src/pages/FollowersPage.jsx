5d345b677980dd35a6f5bdb45d7ab233382a086a
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2022-06-07 20:35:49 -0500

    Feac list followers (#35)
    * feac: create menu to list followers
    
    * Feac: add list by filter
    
    Co-authored-by: Luis porto <lmportoesq@gmail.com>

--- my-campaign/src/pages/FollowersPage.jsx
+++ my-campaign/src/pages/FollowersPage.jsx
@@ -1,38 +1,22 @@
 import GetHome from '../components/GetHome';
 import Footer from '../components/Footer';
+import clienteAxios from '../config/axios';
+import { useEffect, useState } from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-function FollwersPage() {
-    const dataFollowers = [
-        {
-            "cedula": "73154812",
-            "nombre": "Luis Miguel",
-            "ciudad": "Cartagena",
-            "municipio": "Turbaco",
-            "mesa": "23"
-        },
-        {
-            "cedula": "73154856",
-            "nombre": "Jose Perez",
-            "ciudad": "Cartagena",
-            "municipio": "Arjona",
-            "mesa": "2"
-        },
-        {
-            "cedula": "22729384",
-            "nombre": "Claudia Puerta",
-            "ciudad": "Barranquilla",
-            "municipio": "Luruaco",
-            "mesa": "18"
-        },
-        {
-            "cedula": "2567890",
-            "nombre": "Pedro Moreno",
-            "ciudad": "Cartagena",
-            "municipio": "Arjona",
-            "mesa": "2"
-        }
-    ]
+function FollowersPage() {
+    const [data, setData] = useState([]);
+    const idLeader = localStorage.getItem('id');
+    const consultarAPI = async () => {
+        const consultaSeguidores = await clienteAxios.get(`/api/followers?leader=${idLeader}`)
+        const data = consultaSeguidores.data;
+        setData(data);
+    }
+
+    useEffect(() => {
+        consultarAPI();
+    }, []);
 
     return (
         <div className='text-center mx-2'>
@@ -43,26 +27,31 @@ function FollwersPage() {
                     <tr>
                         <th>Cédula</th>
                         <th>Nombre</th>
-                        <th>Ciudad</th>
-                        <th>Municipio</th>
+                        <th>Dirección</th>
+                        <th>Teléfono</th>
                         <th>Mesa</th>
+                        <th>|  Opciones  |</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {dataFollowers.map((item) => (
+                    {data.map((item) => (
                         <tr>
-                            <td>{item.cedula}</td>
-                            <td>{item.nombre}</td>
-                            <td>{item.ciudad}</td>
-                            <td>{item.municipio}</td>
-                            <td>{item.mesa}</td>
+                            <td>{item.docIdent}</td>
+                            <td>{item.firstName} {item.lastName}</td>
+                            <td>{item.adress}</td>
+                            <td>{item.phoneNumber}</td>
+                            <td>{item.votingTable}</td>
+                            <td>
+                                <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-1 m-1" type="submit">Editar</button>
+                                <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold rounded p-1 m-1" type="submit">Eliminar</button>
+                            </td>
                         </tr>
                     ))};
                 </tbody>
             </table>
-            <h2 className='text-right'>Total seguidores: {dataFollowers.length}</h2>            
+            <h2 className='text-right'>Total seguidores: {data.length}</h2>
             <Footer />
         </div>
     )
 }
-export default FollwersPage;
+export default FollowersPage;
