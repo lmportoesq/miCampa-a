bcbb2f1b115e300bbb2323780ced303913262cfa
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2023-04-12 08:23:53 -0500

    feact-changing structure (#50)
    * feact-changing structure
    
    * feact-changing styles
    
    ---------
    
    Co-authored-by: Rita Music <lmportoesq@gmail.com>

---
+++ my-campaign/src/components/create_users/CreateUsers.jsx
@@ -0,0 +1,70 @@
+import Swal from 'sweetalert2';
+import { useState } from 'react';
+import clienteAxios from '../../config/axios';
+
+function CreateUser() {
+    const [data, setData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+    });
+    const handleChange = (e) => {
+        setData({
+            ...data,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await clienteAxios.post('/api/users', data)
+            .then(res => {
+                if (res.data.code === 11000) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Hubo un error',
+                        text: 'Usuario ya fue registrado...',
+                    });
+                } else {
+                    Swal.fire(
+                        'Usuario agregado corréctamente ',
+                        res.data.mensaje,
+                        'success',
+                    );
+                }
+            })
+
+    };
+
+    const handleValidate = () => {
+        const { firstName, lastName, email } = data;
+        const valido = !firstName.length || !lastName.length || !email.length;
+        return valido;
+    };
+
+    return (
+        <>
+            <div form className='form' onSubmit={handleSubmit}>
+                <h1 className="title">Registro de líderes</h1>
+                <hr />
+                <p className="paragraph">
+                    Por favor dilegencia la información solicitada, los campos con asteriscos, indican que no deben quedar en blanco
+                </p>
+                <div className="row">
+                    <p className="label">Nombre del líder:</p>
+                    <input className='input-text' name="firstName" type="text" placeholder="Primer nombre" onChange={handleChange} />
+                </div>
+                <div className="row">
+                    <p className="label">Apellidos:</p>
+                    <input className='input-text' name="firstName" type="text" placeholder="Primer nombre" onChange={handleChange} />
+                </div>
+                <div className="row">
+                    <p className="label">Email:</p>
+                    <input className='input-text' name="email" type="email" placeholder="Email" onChange={handleChange} />
+                </div>
+                <button className="button" type="submit" disabled={handleValidate()}>Enviar</button>
+            </div>
+        </>
+    )
+}
+export default CreateUser;
