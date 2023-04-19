ffe17ef71b2520df8bfe7d2b2d3a46c9d954a554
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2023-04-19 08:45:30 -0500

    feact-adding leaders with users (#54)
    Co-authored-by: Rita Music <lmportoesq@gmail.com>

--- my-campaign/src/components/create_users/CreateUsers.jsx
+++ my-campaign/src/components/create_users/CreateUsers.jsx
@@ -3,46 +3,53 @@ import { useState } from 'react';
 import clienteAxios from '../../config/axios';
 
 function CreateUser() {
-    const profile=JSON.parse(localStorage.getItem('profile'));
+    const profile = JSON.parse(localStorage.getItem('profile'));
 
     const [data, setData] = useState({
         firstName: '',
         lastName: '',
         email: '',
-        campaign:profile.campaign_id
+        campaign: profile.campaign_id,
+        docIdent: '',
+        leaderType: '',
+        phoneNumber: '',
+        adress: '',
+        photo: '',
     });
 
     const handleChange = (e) => {
         setData({
             ...data,
             [e.target.name]: e.target.value,
+
         });
+       // handleValidate();
     };
 
     const handleValidate = () => {
-        const { firstName, lastName, email } = data;
-        const valido = !firstName.length || !lastName.length || !email.length;
+        const { firstName, lastName, email, docIdent, leaderType, phoneNumber, adress } = data;
+        const valido = !firstName.length || !lastName.length || !email.length || !docIdent.length || !leaderType.length || !phoneNumber.length || !adress.length;
         return valido;
     }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        await clienteAxios.post('/api/users',data)
-        .then (res=>{
-            if (res.data.code === 11000) {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Hubo un error',
-                    text: 'Usuario ya fue registrado...',
-                });
-            }else {
-                Swal.fire(
-                    'Usuario agregado corréctamente, se ha enviado un correo para su activación.',
-                    res.data.mensaje,
-                    'success',
-                );
-            }
-        });
+        await clienteAxios.post('/api/users', data)
+            .then(res => {
+                if (res.data.code === 11000) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Hubo un error',
+                        text: 'Usuario ya fue registrado...',
+                    });
+                } else {
+                    Swal.fire(
+                        'Usuario agregado corréctamente, se ha enviado un correo para su activación.',
+                        res.data.mensaje,
+                        'success',
+                    );
+                }
+            });
     }
 
     return (
@@ -54,17 +61,39 @@ function CreateUser() {
                     Por favor dilegencia la información solicitada, los campos con asteriscos, indican que no deben quedar en blanco
                 </p>
                 <div className="row">
-                    <p className="label">Nombre del líder:</p>
+                    <p className="label">*Cédula:</p>
+                    <input className='input-text' type="number" name="docIdent" placeholder="Cédula No." onChange={handleChange} />
+                </div>
+                <div className="row">
+                    <p className="label">*Nombre(s):</p>
                     <input className='input-text' name="firstName" type="text" placeholder="Ingrese nombre" onChange={handleChange} />
                 </div>
                 <div className="row">
-                    <p className="label">Apellidos:</p>
+                    <p className="label">*Apellidos:</p>
                     <input className='input-text' name="lastName" type="text" placeholder="Ingrese apellidos" onChange={handleChange} />
                 </div>
                 <div className="row">
-                    <p className="label">Email:</p>
+                    <p className="label">*Dirección:</p>
+                    <input className='input-text' type="text" name="adress" placeholder="Dirección" onChange={handleChange} />
+                </div>
+                <div className="row">
+                    <p className="label">*Teléfono(s):</p>
+                    <input className='input-text' type="tel" name="phoneNumber" placeholder="Teléfono" onChange={handleChange} />
+                </div>
+                <div className="row">
+                    <p className="label">*Tipo de líder</p>
+                    <select name="leaderType" className='input-text' onChange={handleChange}>
+                        <option value=""></option>
+                        <option value="Aspirante al concejo">Aspirante al concejo</option>
+                        <option value="Comunitario">Comunitario</option>
+                        <option value="Otro">Otro</option>
+                    </select>
+                </div>
+                <div className="row">
+                    <p className="label">*Email:</p>
                     <input className='input-text' name="email" type="email" placeholder="Email" onChange={handleChange} />
                 </div>
+
                 <button className="button" type="submit" disabled={handleValidate()}>Enviar</button>
             </form>
 
