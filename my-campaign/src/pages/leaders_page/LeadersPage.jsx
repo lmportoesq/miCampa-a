4abff0a83d42ac0308d99173cd848b1b8abc8eea
Rita Music <lmportoesq@gmail.com>
2023-04-20 11:29:54 -0500

    feact-create-FollowerList component

--- my-campaign/src/pages/leaders_page/LeadersPage.jsx
+++ my-campaign/src/pages/leaders_page/LeadersPage.jsx
@@ -1,19 +1,17 @@
-import './leaders_page.css';
-
 function LeadersPage({ data }) {
     function handleClick(){
         alert('Diste clic en eliminar...')
     }
     return (
         <>
-            <div className="row-leaders">
+            <div className="row-list">
                 <p className='text-list__bold'>{data.docIdent}</p>
                 <p className='text-list__bold'>{data.firstName} {data.lastName}</p>
                 <p className='text-list'>{data.leaderType}</p>
                 <p className='text-list'>{data.adress}</p>
                 <p className='text-list'>{data.phoneNumber}</p>
                 <p className='text-list'>{data.email}</p>
-                <div className='row-leaders__actions'>
+                <div className='row-list__actions'>
                     <ion-icon name="trash-outline" onClick={handleClick}></ion-icon>
                     <ion-icon name="create-outline"></ion-icon>
                 </div>
