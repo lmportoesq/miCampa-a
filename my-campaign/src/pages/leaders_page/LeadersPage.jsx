6f5d51c27b7a071bb0c8ebee93f0756dc1e7e791
lmportoesq <82108153+lmportoesq@users.noreply.github.com>
2023-05-04 21:07:47 -0500

    feact delete users (#58)
    Co-authored-by: Rita Music <lmportoesq@gmail.com>

--- my-campaign/src/pages/leaders_page/LeadersPage.jsx
+++ my-campaign/src/pages/leaders_page/LeadersPage.jsx
@@ -1,7 +1,38 @@
+import clienteAxios from '../../config/axios';
+import Swal from "sweetalert2";
+
 function LeadersPage({ data }) {
-    function handleClick(){
-        alert('Diste clic en eliminar...')
+
+    const confirmDelete = () => {
+        Swal.fire({
+            title: 'Eliminar',
+            text: "¿Esta seguro de eliminar el usuario?",
+            icon: 'warning',
+            showCancelButton: true,
+            cancelButtonColor: '#d33',
+            cancelButtonText: 'No',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Si'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                handlerDelete();
+            }
+        })
     }
+
+    const handlerDelete = async () => {
+        const id = data._id;
+        try {
+            const res = await clienteAxios.delete(`/api/users/${id}`);
+            if (res.status === 200) {
+                window.location.reload();
+                //Swal.fire({ text: "Cliente se ha eliminado corréctamente...", icon: "success", timer: "3000" })
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <div className="row-list">
@@ -12,7 +43,7 @@ function LeadersPage({ data }) {
                 <p className='text-list'>{data.phoneNumber}</p>
                 <p className='text-list'>{data.email}</p>
                 <div className='row-list__actions'>
-                    <ion-icon name="trash-outline" onClick={handleClick}></ion-icon>
+                    <ion-icon name="trash-outline" onClick={confirmDelete}></ion-icon>
                     <ion-icon name="create-outline"></ion-icon>
                 </div>
             </div>
